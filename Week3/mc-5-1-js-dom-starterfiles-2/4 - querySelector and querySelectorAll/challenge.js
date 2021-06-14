981993c1b2cba749bd56a718fc6ff477bcc633d4
Carly Morris <thabai@hotmail.com>
2021-06-14 15:39:06 +0100

    commit

--- Week3/mc-5-1-js-dom-starterfiles-2/4 - querySelector and querySelectorAll/challenge.js
+++ Week3/mc-5-1-js-dom-starterfiles-2/4 - querySelector and querySelectorAll/challenge.js
@@ -1,2 +1,19 @@
+
+// Use a for loop to display all even items to have text colour “lightgreen”
+const listItems = document.querySelectorAll("li:nth-child(even)"); 
+console.log(listItems); 
+console.log(listItems.length);
+
+for (let i=0; i <listItems.length; i++){
+    listItems[i].style.color="lightgreen";
+}
+
+
 //cycle over the list items and apply colors from the array called colours
 const colours = ['red', 'yellow', 'goldenrod', 'lightgreen', 'blue', 'purple'];
+
+const list = document.querySelectorAll("li"); 
+
+for (let i=0; i <list.length; i++){
+    list[i].style.color= colours[i];
+}
