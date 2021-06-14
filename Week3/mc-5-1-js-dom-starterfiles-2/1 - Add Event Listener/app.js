f7c0edd9dd25ea443ef1f64f280d5d5009840b2c
Carly Morris <thabai@hotmail.com>
2021-06-14 14:12:11 +0100

    third commit

---
+++ Week3/mc-5-1-js-dom-starterfiles-2/1 - Add Event Listener/app.js
@@ -0,0 +1,19 @@
+const myHeading = document.getElementById("heading");
+
+myHeading.addEventListener("click", ()=>{
+    myHeading.style.color = "red";
+});
+
+const myInput = document.getElementById("input");
+
+myInput.addEventListener("click", ()=>{
+    myInput.style.color = "green";
+});
+
+const myButton = document.getElementById("button");
+
+myButton.addEventListener("click", ()=>{
+    myButton.style.color = "red";
+});
+
+
---
+++ Week3/mc-5-1-js-dom-starterfiles-2/2 - Selecting an Element By Its ID/app.js
@@ -0,0 +1,9 @@
+const myHeading = document.getElementById("heading");
+const myInput = document.getElementById("input");
+const myButton = document.getElementById("button");
+
+myButton.addEventListener("click", ()=>{
+    myHeading.style.color = myInput.value;
+});
+
+//get value from input value and change heading colour
