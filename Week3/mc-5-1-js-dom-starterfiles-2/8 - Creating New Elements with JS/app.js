65001c0a962d9261d12cb3f4756f0cb32a7a8dc3
Carly Morris <thabai@hotmail.com>
2021-06-15 15:46:50 +0100

    commit

--- Week3/mc-5-1-js-dom-starterfiles-2/8 - Creating New Elements with JS/app.js
+++ Week3/mc-5-1-js-dom-starterfiles-2/8 - Creating New Elements with JS/app.js
@@ -1,8 +1,11 @@
 // create a “list” item when the person presses the submit button.
+// Challenge -Clear the input field when the user presses the “submit” 
+// button. Add feature where user can show/hide the list.
 
 const button= document.getElementById("submit"); 
 const input = document.getElementById("input");
-let list = document.getElementsByTagName("ul")[0];
+const buttonTog = document.getElementById("showhide-btn");
+const list = document.getElementsByTagName("ul")[0];
 
 button.addEventListener("click", ()=> {
 let listItem = document.createElement("li");
@@ -11,17 +14,13 @@ list.appendChild(listItem);
 input.value = "";
 })
 
-
-
-const lists = document.getElementById("list");
-const buttonTog = document.getElementById("showhide-btn");
     
 buttonTog.addEventListener("click", () => { 
-    if(lists.style.display == "block"){
-      lists.style.display = "none";
+    if(list.style.display == "block"){
+      list.style.display = "none";
       buttonTog.textContent = "show";
     } else {
-      lists.style.display = "block";
+      list.style.display = "block";
       buttonTog.textContent = "hide";
     }
     });
---
+++ Week3/mc-5-1-js-dom-starterfiles-2/9 - Removing Elements/app.js
@@ -0,0 +1,19 @@
+const button= document.getElementById("submit"); 
+const input = document.getElementById("input");
+const removeBtn = document.getElementById("remove");
+
+button.addEventListener("click", ()=> {
+let listItem = document.createElement("li");
+listItem.textContent = input.value;
+list.appendChild(listItem);
+input.value = "";
+})
+
+
+removeBtn.addEventListener("click", ()=> {
+let lastItem = document.querySelector('li:last-child');
+let list = document.getElementsByTagName('ul')[0];    
+
+list.removeChild(lastItem);
+})
+
