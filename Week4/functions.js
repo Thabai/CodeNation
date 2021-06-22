eb374a7a68112fe1dab8ec1a71d3a9f78f6c4889
Carly Morris <thabai@hotmail.com>
2021-06-22 16:03:46 +0100

    commit

--- Week4/functions.js
+++ Week4/functions.js
@@ -16,44 +16,25 @@ const getUsers = () => {
 // addUser('charlie', getUsers)
 
 //promise
-const addUser = (username) => {
-    return new Promise((resolve, reject) => {
+// const addUser = (username) => {
+//     return new Promise((resolve, reject) => {
 
-        setTimeout(() => {
-            users.push(username)
+//         setTimeout(() => {
+//             users.push(username)
 
-            let error = false
+//             let error = false
 
-            if(!error){
-                resolve()
-            } else {
-                reject('there is an error')
-            }
-        },2000)
+//             if(!error){
+//                 resolve()
+//             } else {
+//                 reject('there is an error')
+//             }
+//         },2000)
 
-    })
-}
-addUser('charlie')
-.then(getUsers)
-.catch((err) => {console.log(err)})
+//     })
+// }
+// addUser('charlie')
+// .then(getUsers)
+// .catch((err) => {console.log(err)})
 
 
-let movies = ["Chopper", "ID", "Scum", "12 Hour Shift"];
-const removeMovie = () =>{
-    movies.pop(movies);
-    console.log(movies);
-    return movies;
-}
-console.log(movies);
-removeMovie();
-let movieToAdd = "The Lost Boys";
-    const addMovie = () =>{
-        movies.push(movieToAdd);
-        console.log(movies)
-        setTimeout(() => {
-            console.log(movies);
-        }, 3000);
-        removeMovie("Chopper");
-        console.log(movies);
-    }
-addMovie();
